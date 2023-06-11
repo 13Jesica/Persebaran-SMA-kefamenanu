12b8449cfabd9060c208707b53bb1522aeebfdfe
13Jesica <136223600+13Jesica@users.noreply.github.com>
2023-06-11 13:56:55 +0700

    Add files via upload

---
+++ data/SMADIKOTAKEFAMENANU_1.js
@@ -0,0 +1,16 @@
+var json_SMADIKOTAKEFAMENANU_1 = {
+"type": "FeatureCollection",
+"name": "SMADIKOTAKEFAMENANU_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "NM SEKOLAH": "SMAN TAEKAS", "FOTO": "fotoooo sma/SMAN TAEKAS.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.479751886777237, -9.414405812115152 ] } },
+{ "type": "Feature", "properties": { "id": "2", "NM SEKOLAH": "SMEA KEFA", "FOTO": "fotoooo sma/SMEA KEFA.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.47880391061048, -9.43750397237781 ] } },
+{ "type": "Feature", "properties": { "id": "3", "NM SEKOLAH": "SMAK WARTA BAKTI", "FOTO": "fotoooo sma/SMAK WARTA BAKTI.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.474726900329486, -9.442102325599633 ] } },
+{ "type": "Feature", "properties": { "id": "4", "NM SEKOLAH": "SMAN 1 KEFA", "FOTO": "fotoooo sma/SMAN 1 KEFA.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.483023473698523, -9.456220433213302 ] } },
+{ "type": "Feature", "properties": { "id": "5", "NM SEKOLAH": "SMK INTAN KEFA", "FOTO": "fotoooo sma/smk intan.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.495724928804634, -9.469634910997637 ] } },
+{ "type": "Feature", "properties": { "id": "6", "NM SEKOLAH": "SMAN 3 BANSONE", "FOTO": "fotoooo sma/SMAN 3 KEFA.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.465660541802663, -9.44280543230418 ] } },
+{ "type": "Feature", "properties": { "id": "7", "NM SEKOLAH": "SMK CLARENT KEFA", "FOTO": "fotoooo sma/SMK CLARENT.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.480901218783984, -9.467197072477591 ] } },
+{ "type": "Feature", "properties": { "id": "8", "NM SEKOLAH": "SMAK FIDES", "FOTO": "fotoooo sma/SMAN 3 KEFA.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.495366764877133, -9.503512394570949 ] } },
+{ "type": "Feature", "properties": { "id": "9", "NM SEKOLAH": "SMAN 2 KEFA", "FOTO": "fotoooo sma/SMA 2 KEFA.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.498151890344388, -9.48746382522585 ] } }
+]
+}
